9b55714cef6b633e33ac06f8db31ba009670d2c5
wangyunlai <hnwyllmm@126.com>
2025-12-19 16:27:29 +0800

    support pages

---
+++ docs/api/index.rst
@@ -0,0 +1,45 @@
+API Reference
+=============
+
+This page contains the auto-generated API reference for pyseekdb.
+
+Main Package
+------------
+
+.. automodule:: pyseekdb
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Client Modules
+--------------
+
+.. autosummary::
+   :toctree: generated
+   :recursive:
+   :template: module.rst
+
+   pyseekdb.client
+   pyseekdb.client.client_base
+   pyseekdb.client.client_seekdb_embedded
+   pyseekdb.client.client_seekdb_server
+   pyseekdb.client.base_connection
+   pyseekdb.client.collection
+   pyseekdb.client.configuration
+   pyseekdb.client.embedding_function
+   pyseekdb.client.hybrid_search
+   pyseekdb.client.admin_client
+   pyseekdb.client.database
+   pyseekdb.client.filters
+   pyseekdb.client.meta_info
+   pyseekdb.client.sql_utils
+   pyseekdb.client.version
+
+Utility Modules
+---------------
+
+.. autosummary::
+   :toctree: generated
+   :recursive:
+
+   pyseekdb.utils.embedding_functions.sentence_transformer_embedding_function
---
+++ docs/conf.py
@@ -0,0 +1,47 @@
+"""Sphinx configuration file for pyseekdb documentation."""
+
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
+
+# Project information
+project = "pyseekdb"
+copyright = "2025, OceanBase"
+author = "OceanBase <open_oceanbase@oceanbase.com>"
+
+try:
+    import importlib.metadata
+    release = importlib.metadata.version("pyseekdb")
+except importlib.metadata.PackageNotFoundError:
+    release = "0.0.1.dev1"
+
+version = release
+
+# Extensions
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+]
+
+# Autodoc settings
+autodoc_default_options = {
+    "members": True,
+    "show-inheritance": True,
+}
+
+autosummary_generate = True
+autosummary_imported_members = True
+autodoc_mock_imports = ["pylibseekdb", "pymysql", "onnxruntime", "tokenizers", "httpx", "tqdm", "tenacity", "numpy"]
+
+# Source files
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
+master_doc = "index"
+exclude_patterns = ["_build"]
+
+# HTML theme
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
